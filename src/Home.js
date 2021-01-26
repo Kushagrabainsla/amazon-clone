2ee5bf8a790d86014c7f4329783516cec92c8ab0
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-01-26 22:37:15 +0530

    Home page done

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Home.css";
-import Product from "./Product,js";
+import Product from "./Product.js";
 
 function Home() {
     return (
@@ -10,20 +10,49 @@ function Home() {
                     src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt=""/>
 
                 <div className="home__row">
-                    <Product/>
-                    <Product/>
+                    <Product 
+                        title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses" 
+                        price={320} 
+                        rating={5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg" 
+                    />
+                    <Product 
+                        title="Hooked: How to Build Habit-Forming Products" 
+                        price={447} 
+                        rating={5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51DlnjccG-L._SX322_BO1,204,203,200_.jpg" 
+                    />
                 </div>
-
                 <div className="home__row">
-                    <Product/>
-                    <Product/>
-                    <Product/>
-
+                    <Product 
+                        title="Can't Hurt Me: Master Your Mind and Defy the Odds" 
+                        price={720} 
+                        rating={5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/410MI8gKwzL._SX331_BO1,204,203,200_.jpg"
+                    />
+                    <Product 
+                        title="Atomic Habits: The life-changing million copy bestseller" 
+                        price={400} 
+                        rating={5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg"
+                    />
+                    <Product 
+                        title="To Sell is Human: The Surprising Truth About Persuading, Convincing, and Influencing Others" 
+                        price={260} 
+                        rating={5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51xM8OfmPGL._SX324_BO1,204,203,200_.jpg"
+                    />
                 </div>
-
                 <div className="home__row">
-                    <Product/>
+                    <Product 
+                        title="Seagate Expansion 1.5 TB External HDD - USB 3.0 for PC Laptop, 3 yr Data Recovery Services, Portable Hard Drive (STEA1500400)" 
+                        price={4300} 
+                        rating={5}
+                        image="https://m.media-amazon.com/images/I/71p2W9Ykh7L._AC_AA180_.jpg"
+                    />
                 </div>
+
+
             </div>
         </div>
     );
--- src/Product.js
+++ src/Product.js
@@ -2,14 +2,14 @@
 import React from "react";
 import "./Product.css";
 
-function Product({ id, title, image, price, rating }) {
+function Product({ title, image, price, rating }) {
  
   return (
     <div className="product">
       <div className="product__info">
         <p>{title}</p>
         <p className="product__price">
-          <small>$</small>
+          <small>₹</small>
           <strong>{price}</strong>
         </p>
         <div className="product__rating">
