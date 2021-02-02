68404696f07f34cb8f833ab13564556676cb3cb6
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-02-02 18:44:41 +0530

    header changes

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
 
 function Header() {
-    const [{basket, user}, dispatch] = useStateValue();
+    const [{basket, user}] = useStateValue();
     var ind = user?.email.indexOf("@");
 
     const handleAuthenticaton = () => {
