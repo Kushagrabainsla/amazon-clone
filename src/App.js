4a6773c309be55d5950f70bad3e9481d1bc6aa77
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-03-12 23:52:43 +0530

    n

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
   const [{}, dispatch] = useStateValue();
 
   useEffect(() => {
-    // will only run once when the app component loads...
 
     auth.onAuthStateChanged((authUser) => {
       console.log("THE USER IS >>> ", authUser);
